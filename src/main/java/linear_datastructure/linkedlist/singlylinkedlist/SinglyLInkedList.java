7703ac60b9b560b389a9e1972dda8cd59957e057
ARU0007 <aruneshagrahari95@gmail.com>
2024-03-19 13:49:53 +0530

    created a method creation.

--- src/main/java/linear_datastructure/linkedlist/singlylinkedlist/SinglyLInkedList.java
+++ src/main/java/linear_datastructure/linkedlist/singlylinkedlist/SinglyLInkedList.java
@@ -4,6 +4,27 @@ import java.util.Scanner;
 public class SinglyLInkedList {
     Node head = null;
 
+    //    creating functions------------------------------>
+    public void creation() {
+        int n;
+        int data;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Enter the data");
+            data = sc.nextInt();
+            Node new_node = new Node(data);
+            if (head == null) {
+                head = new_node;
+            } else {
+                new_node.next = head;
+                head = new_node;
+            }
+            System.out.println("Do you want to add more data, Press-1");
+            n = sc.nextInt();
+        } while (n == 1);
+
+    }
+
 
 
     static class Node {
