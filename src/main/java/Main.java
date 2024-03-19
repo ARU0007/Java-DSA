febfb5d55a44c99086cf647febc2f53ecfa4e2dd
ARU0007 <aruneshagrahari95@gmail.com>
2024-03-19 13:48:20 +0530

    created Main.java.

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+package main.java;
+
+import main.java.linear_datastructure.linkedlist.singlylinkedlist.SinglyLInkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
