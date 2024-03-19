41d650acb5bcd1df4dff45ae6049586789e1f824
ARU0007 <aruneshagrahari95@gmail.com>
2024-03-19 17:05:49 +0530

    created a class node inside SLLGenerics.java.

--- src/main/java/linear_datastructure/linkedlist/singlylinkedlist/generics/SLLGenerics.java
+++ src/main/java/linear_datastructure/linkedlist/singlylinkedlist/generics/SLLGenerics.java
@@ -1,6 +1,29 @@
 package main.java.linear_datastructure.linkedlist.singlylinkedlist.generics;
 
 
+
+import java.util.Scanner;
+
 public class SLLGenerics<T> {
+    Node<T> head = null;
+
+
+
+
+
+    static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+
+        }
+
+
+    }
 
 }
+
+
