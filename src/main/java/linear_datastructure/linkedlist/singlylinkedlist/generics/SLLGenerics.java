7181001b8499d00594ace6a14b6c6dd12aa5c52c
ARU0007 <aruneshagrahari95@gmail.com>
2024-03-19 17:07:05 +0530

    created a method creation .

--- src/main/java/linear_datastructure/linkedlist/singlylinkedlist/generics/SLLGenerics.java
+++ src/main/java/linear_datastructure/linkedlist/singlylinkedlist/generics/SLLGenerics.java
@@ -7,6 +7,32 @@ import java.util.Scanner;
 public class SLLGenerics<T> {
     Node<T> head = null;
 
+    //creating functions.
+    //creation.
+
+    public void create() {
+        int n;
+        T data;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Enter the data");
+            data = (T) sc.next();
+            Node<T> new_node = new Node<>(data);
+            if (head == null) {
+                head = new_node;
+            } else {
+                new_node.next = head;
+                head = new_node;
+            }
+            System.out.println("enter 1 for more data");
+            n = sc.nextInt();
+
+        } while (n == 1);
+    }
+
+
+
+
 
 
 
