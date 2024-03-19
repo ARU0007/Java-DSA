74b4ae18ffc69abe7f55f933e6f303f9c180df10
ARU0007 <aruneshagrahari95@gmail.com>
2024-03-19 13:49:07 +0530

    created SinglyLInkedList.java for implimentation.

---
+++ src/main/java/linear_datastructure/linkedlist/singlylinkedlist/SinglyLInkedList.java
@@ -0,0 +1,19 @@
+package main.java.linear_datastructure.linkedlist.singlylinkedlist;
+import java.util.Scanner;
+
+public class SinglyLInkedList {
+    Node head = null;
+
+
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
