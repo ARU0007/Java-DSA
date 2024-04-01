cf17b42671b41d2caa3dd235f41b6fde2f810f49
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-01 11:29:23 +0530

    created interface and methos for stack. .

---
+++ src/main/java/linear_datastructure/Stack/adt/Stack.java
@@ -0,0 +1,17 @@
+package main.java.linear_datastructure.Stack.adt;
+
+public interface Stack {
+
+//Methos for Stack''''''''''''''''''''''''''''''''''''''''''''''''''
+
+    void push(int data);
+
+    int pop();
+
+    int peek();
+
+    int getsize();
+
+    boolean isunderflow();
+
+}
