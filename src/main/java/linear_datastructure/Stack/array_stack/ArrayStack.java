0107700cedb6834065907297a4d682d53d075448
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-01 11:31:56 +0530

    created a class ArrayStack.java. .

---
+++ src/main/java/linear_datastructure/Stack/array_stack/ArrayStack.java
@@ -0,0 +1,57 @@
+package main.java.linear_datastructure.Stack.array_stack;
+
+import main.java.linear_datastructure.Stack.adt.Stack;
+
+public class ArrayStack implements Stack {
+    public int[] array;
+    int top;
+    int size;
+
+    public ArrayStack(int capacity) {
+        this.array = new int[capacity];
+        this.top = -1;
+        this.size = 0;
+
+    }
+
+    //override the methods''''''''''''''''''''''''''''''''''''''''''''
+
+    @Override
+    public void push(int data) {
+        if(top==array.length -1){                     //check for overflow
+            System.err.println("Stack overflow");     //return error statement
+        }
+        top++;                      //else
+        array[top]=data;
+        size++;
+
+    }
+
+    @Override
+    public int pop() {
+        if (isunderflow()) {                           //check for underflow
+            System.err.println("Stack underflow");
+            return Integer.MIN_VALUE;                  //return Min value
+        }
+        int popedvalue = array[top];                   //else
+        array[top--] = 0;
+        size--;
+        return popedvalue;
+
+    }
+
+    @Override
+    public int peek() {
+        if(isunderflow()){                                  //check for underflow
+            System.err.println("Stack Underflow");
+            return Integer.MIN_VALUE;                      //return Min value
+        }
+        return array[top];
+    }
+
+    @Override
+    public int getsize(){ return size;}         //retuen the Size of stack
+
+    @Override
+    public boolean isunderflow() { return top == -1; }  //return -1 if Stack is underflow
+}
