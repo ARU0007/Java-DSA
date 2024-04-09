9b1eae60072e6431ae2ff795f2cd28c709ed0e87
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-09 19:19:30 +0530

    created a factorial method.

---
+++ src/main/java/recursion/Factorial.java
@@ -0,0 +1,11 @@
+package main.java.recursion;
+
+public class Factorial {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
