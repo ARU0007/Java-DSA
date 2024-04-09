24445a7fe92041f9064458ddd3458534500d5a2a
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-09 18:34:11 +0530

    created a constructor .

--- src/main/java/linear_datastructure/queue/LinkedListQueue.java
+++ src/main/java/linear_datastructure/queue/LinkedListQueue.java
@@ -1,6 +1,18 @@
 package main.java.linear_datastructure.queue;
 
 public class LinkedListQueue {
+    int size;
+    private Node front; //Queue's front data.
+    private Node rear; //Quese's rear.
+
+    //constructer to initialize Queue.
+    public LinkedListQueue() {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
+
+    }
+
 
 
     private static class Node {
