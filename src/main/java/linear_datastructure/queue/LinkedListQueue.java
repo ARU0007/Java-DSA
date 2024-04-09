2070aa15e35163cc44ffdb2d4913f072b6cff5a8
ARU0007 <aruneshagrahari95@gmail.com>
2024-04-09 18:32:53 +0530

    created a Node class .

---
+++ src/main/java/linear_datastructure/queue/LinkedListQueue.java
@@ -0,0 +1,16 @@
+package main.java.linear_datastructure.queue;
+
+public class LinkedListQueue {
+
+
+    private static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+}
